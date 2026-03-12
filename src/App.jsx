388a0036acdd729ab7a05540adc39cdf8b42d2bd
rafael <rafa.cachobarros@gmail.com>
2026-03-11 21:17:30 -0400

    interruptor pronto

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import { useState } from 'react'
 import './style.scss'
 
+import ligado from "./assets/ligado.png";
+import desligado from "./assets/desligado.png";
+
 function App() {
   const [ligar, setLigar] = useState(false)
 
-  console.log(ligar);
-
   return (
-    <>
-      <body className={`${ligar ? "luz" : "breu"}`}>
-        <div>
-          <button onClick={() => setLigar(!ligar)}>
-            {`${ligar ? "Apagar a Luz" : "Ligar a Luz"}`}
-          </button>
-        </div>
-      </body>
-    </>
+    <div className={`container ${ligar ? "luz" : "breu"}`}>
+      {ligar ? <img className='interruptor' src={ligado} onClick={() => setLigar(!ligar)} alt="Interruptor" /> : <img className='interruptor' src={desligado} onClick={() => setLigar(!ligar)} alt="Interruptor" />}
+    </div>
   )
 }
 
